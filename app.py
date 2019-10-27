53512e9a695c2973e9e76af51372d704838913fa
mollha <molly.hayward@durham.ac.uk>
2019-10-27 21:18:13 +0000

    Added basic Flask template

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-imo
+from flask import Flask
+
+# set up application referencing the file
+app = Flask(__name__)
+
+# set up index route
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
